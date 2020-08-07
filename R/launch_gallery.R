e3662f31966e06c945e59305d898e8f92be3acde
davidruvolo51 <davidruvolo51@gmail.com>
2020-08-07 14:09:58 +0200

    removed shiny from NAMESPACE

--- R/launch_gallery.R
+++ R/launch_gallery.R
@@ -3,11 +3,11 @@
 #' This function starts the rheroicons gallery
 #' @return This function starts the rheroicons gallery
 #' @usage rheroicons::launch_gallery()
-#' @importFrom shiny runApp
-#' @keywords rheroicons launch_gallery demo
+#' @keywords rheroicons gallery demo
+#'
 #' @export
 launch_gallery <- function() {
     path <- system.file("rheroicons-demo", package = "rheroicons")
     if (path == "") stop("Demo app does not exist.", call. = FALSE)
-    runApp(path, display.mode = "normal")
+    shiny::runApp(path, display.mode = "normal")
 }
