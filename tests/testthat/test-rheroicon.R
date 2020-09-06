89e4e1208861de0cb0324826593fa9f11bc116ff
davidruvolo51 <davidruvolo51@gmail.com>
2020-09-06 18:47:44 +0200

    updated unit tests to match new functions

---
+++ tests/testthat/test-rheroicon.R
@@ -0,0 +1,66 @@
+#' ////////////////////////////////////////////////////////////////////////////
+#' FILE: test-rheroicon.R
+#' AUTHOR: David Ruvolo
+#' CREATED: 2020-09-06
+#' MODIFIED: 2020-09-06
+#' PURPOSE: tests for the function `rheroicon`
+#' STATUS: in.progress
+#' PACKAGES: usethis; rheroicons;
+#' COMMENTS: NA
+#' ////////////////////////////////////////////////////////////////////////////
+
+
+#' Class evaluation
+#'
+#' Does the default output of the `rheroicon` function return an HTML
+#' element? The SVG icons are stored in an internal dataset as SVG
+#' character strings. When used in web-based project (i.e., shiny),
+#' the output should have the class `HTML`
+test_that("rheroicon class", {
+    expect_equal(
+        object = class(rheroicon(name = "academic_cap"))[1],
+        expected = "html",
+        label = "Output is not an HTML element"
+    )
+})
+
+
+#' Icon Type
+#'
+#' If the argument `type` is used, is the appropriate icon returned?
+#' There are two styles to choose from (solid and outline). Each icon
+#' has a slightly different markup and can be evaluated using the
+#' class attribute.
+test_that("icon type", {
+    expect_equal(
+        object = c(
+            stringr::str_match(
+                string = rheroicon(name = "rss", type = "outline"),
+                pattern = "rheroicons_outline"
+            )[1],
+            stringr::str_match(
+                string = rheroicon(name = "rss", type = "solid"),
+                pattern = "rheroicons_solid"
+            )[1]
+        ),
+        expected = c("rheroicons_outline", "rheroicons_solid"),
+        label = "Icon types are not properly rendered"
+    )
+})
+
+
+
+#' Classnames
+#'
+#' If the argument `classnames` is used, are CSS classes properly
+#' appended to the class attribute?
+test_that("classnames", {
+    expect_equal(
+        object = stringr::str_extract(
+            string = rheroicon(name = "map", classnames = "my-icons"),
+            pattern = "class=.([\\w\\s-])+."
+        ),
+        expected = "class=\"rheroicons rheroicons_outline rheroicons_map my-icons\"",
+        label = "css classnames are not properly added"
+    )
+})
