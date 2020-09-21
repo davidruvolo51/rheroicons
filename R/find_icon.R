94a817c81a067ad7834df7a357fa0658abd6ca9c
davidruvolo51 <davidruvolo51@gmail.com>
2020-09-21 17:52:06 +0200

    new find icon feature

---
+++ R/find_icon.R
@@ -0,0 +1,29 @@
+#' Find Icons
+#'
+#' Find an icon using a search term
+#'
+#' @param query a search term passed down to \code{stringr::str_subset}
+#'
+#' @examples
+#' find_icons(query = "chevron")
+#' find_icons(query = "chevron|arrow")
+#' find_icons(query = "down|up|left|right")
+#'
+#' @return Find an icon using a search term
+#'
+#' @export
+find_icons <- function(query) {
+
+    if (is.null(query)) {
+        cli::cli_alert_danger(
+            text = "'query' is undefined"
+        )
+    }
+
+    if (!is.null(query)) {
+        stringr::str_subset(
+            string = names(rheroicons),
+            pattern = query
+        )
+    }
+}
