26133032727bb4772557656a0a06a36b38703a94
davidruvolo51 <davidruvolo51@gmail.com>
2021-01-16 15:20:34 +0100

    updated  dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 // FILE: webpack.config.js
 // AUTHOR: David Ruvolo
 // CREATED: 2020-09-24
-// MODIFIED: 2020-09-24
+// MODIFIED: 2021-01-16
 // PURPOSE: loads dev or prod configuration based on script param `env`
 // DEPENDENCIES: see common
 // STATUS: working
@@ -15,6 +15,7 @@ const commonConfig = require("./config/webpack.common");
 
 // load config based on environment
 module.exports = (env) => {
-    const config = require("./config/webpack." + env);
+    const e = env.prod ? "prod" : "dev";
+    const config = require("./config/webpack." + e);
     return merge(commonConfig, config);
 }
